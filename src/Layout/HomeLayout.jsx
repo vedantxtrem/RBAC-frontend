40433865222c64a87debad49c4fa17298072eb83
vedant <vedant@ssipmt.com>
2024-11-24 08:40:04 +0530

    fixing color ratio

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar'
 
 function HomeLayout({childern}) {
   return (
-    <div className='w-screen min-h-screen bg-gradient-to-br from-gray-300 via-gray-200 to-amber-200'>
+    <div className='w-screen min-h-screen bg-gradient-to-r from-gray-300  to-amber-100'>
         <div className='fixed top-4 lg:left-4'>
             <Sidebar/>
         </div>
