96c67cfe3f3ed52461d7174feedb8c66267be69a
vedant <vedant@ssipmt.com>
2024-11-24 08:16:21 +0530

    color

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar'
 
 function HomeLayout({childern}) {
   return (
-    <div className='w-screen min-h-screen bg-gradient-to-br from-gray-300  via-gray-100 to-yellow-200'>
+    <div className='w-screen min-h-screen bg-gradient-to-br from-gray-300 via-gray-200 to-amber-200'>
         <div className='fixed top-4 lg:left-4'>
             <Sidebar/>
         </div>
