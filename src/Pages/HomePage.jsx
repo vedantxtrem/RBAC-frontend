4be46adea04fbbc48f5dc2ed3b0ba10006231fe4
vedant <vedant@ssipmt.com>
2024-11-27 00:00:08 +0530

    fix

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -117,6 +117,22 @@ const HomePage = () => {
     );
   };
 
+  
+  if (loading) {
+    return (
+      <HomeLayout>
+        <div className="w-screen h-screen flex flex-col justify-center items-center text-center text-gray-700">
+          <div class="flex items-center justify-center h-screen">
+            <div class="relative">
+              <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-300"></div>
+              <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-amber-500 animate-spin">
+              </div>
+            </div>
+          </div>
+        </div>
+      </HomeLayout>
+    );
+  }
 
   return (
     <HomeLayout>
